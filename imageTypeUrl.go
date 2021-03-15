a513d77e67ab2f0a6fe22dfa2cd615a81656eb26
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-03-14 21:14:00 -0700

    Updating destination image dir to support partitioning

--- imageTypeUrl.go
+++ imageTypeUrl.go
@@ -2,26 +2,18 @@ package vangogh_urls
 
 import (
 	"fmt"
-	"github.com/arelate/vangogh_types"
 	"path"
 )
 
 const imagesDir = "images"
 
-var imageTypeRootDirs = map[vangogh_types.ImageType]string{
-	vangogh_types.Image:            imagesDir,
-	vangogh_types.BoxArt:           imagesDir,
-	vangogh_types.Background:       imagesDir,
-	vangogh_types.GalaxyBackground: imagesDir,
-	vangogh_types.Logo:             imagesDir,
-	vangogh_types.Icon:             imagesDir,
-	vangogh_types.Screenshots:      imagesDir,
-}
-
-func DstImageTypeUrl(it vangogh_types.ImageType) (string, error) {
-	if !vangogh_types.ValidImageType(it) {
-		return "", fmt.Errorf("vangogh_urls: no local destination for %s", it)
+func DstImageUrl(imageId string) (string, error) {
+	if imageId == "" {
+		return "", fmt.Errorf("vangogh_urls: empty image-id")
+	}
+	if len(imageId) < 2 {
+		return "", fmt.Errorf("vangogh_urls: image-id is too short")
 	}
 
-	return path.Join(imageTypeRootDirs[it], it.String()), nil
+	return path.Join(imagesDir, imageId[0:2]), nil
 }
