28001ff11ff23e0c52318d047504b8db0d692ad4
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-04-02 20:28:12 -0700

    Changing local imageIds enumeration to stop considering .download files as present

--- image_ids.go
+++ image_ids.go
@@ -11,8 +11,10 @@ func LocalImageIds() (imageIds map[string]bool, err error) {
 	imageIds = make(map[string]bool, 0)
 
 	err = filepath.WalkDir(RootImagesDir, func(p string, _ fs.DirEntry, err error) error {
-		_, imageId := path.Split(p)
-		imageIds[strings.TrimSuffix(imageId, path.Ext(imageId))] = true
+		_, imageIdFile := path.Split(p)
+		if !strings.HasSuffix(imageIdFile, ".download") {
+			imageIds[strings.TrimSuffix(imageIdFile, path.Ext(imageIdFile))] = true
+		}
 		return err
 	})
 
